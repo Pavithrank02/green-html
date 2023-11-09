c8b7d7ee3bd7b55658ac39daa1186d9f5f6b6e42
pavithrank02 <pavithrank02@gmail.com>
2023-11-09 12:20:47 +0530

    animation included

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
-// Define the initial state variables
-let isTransition = false;
-let isTransitionLeft = animationName; // You should set this to your desired initial value
-
-// Function to start the animation
-function startAnimation() {
-  isTransition = !isTransition;
-  isTransitionLeft = !isTransitionLeft;
-
-  // You can also call startAnimations() here if needed
-}
-
-// Add a click event listener to the button
-document.getElementById("startAnimation").addEventListener("click", startAnimation);
+document.addEventListener("DOMContentLoaded", function() {
+  const images = document.querySelectorAll(".moving-image");
+  const side = document.querySelectorAll(".img-");
+  const moveRightButton = document.getElementById("moveRight");
+  
+  let isMovingRight = false;
+  let isTransition = false;
+  moveRightButton.addEventListener("click", function() {
+      isMovingRight = !isMovingRight;
+      isTransition = !isTransition;
+      const animation = isMovingRight ? "moveRight" : "moveLeft";
+      const animationNames = isTransition ? "small-image" : "";
+      console.log(animationNames, isTransition)
+      side.forEach(image => {
+        image.style.animationName = animationNames;
+        void image.offsetWidth; // Trigger reflow to restart the animation
+    });
+      images.forEach(image => {
+          image.style.animationName = animation;
+          void image.offsetWidth; // Trigger reflow to restart the animation
+      });
+  });
+});
